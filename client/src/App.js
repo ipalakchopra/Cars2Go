a17e66587184cc25461836efe1e581f25ad8d4fa
ipalakchopra <palak.chopra@somaiya.edu>
2022-11-13 10:05:56 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component }  from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Route, BrowserRouter, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import BookingCar from './pages/BookingCar'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/bookingcar' element={<BookingCar/>} />
+        </Routes>
+      </BrowserRouter>
+
+      
+    </div>
+  );
+}
+
+export default App;
