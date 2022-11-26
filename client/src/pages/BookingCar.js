6c6bca63e391e38267a74e7912e48faef166a73d
arvindias <arvin.dias@somaiya.edu>
2022-11-26 12:28:45 +0530

    payment gateway frontend and backend

--- client/src/pages/BookingCar.js
+++ client/src/pages/BookingCar.js
@@ -1,28 +1,29 @@
-import {Col, Row, Divider, DatePicker, Checkbox, Modal} from 'antd'
+import { Col, Row, Divider, DatePicker, Checkbox, Modal } from 'antd'
 import React, { useState, useEffect } from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import DefaultLayout from '../components/DefaultLayout'
 import { Link, useParams } from 'react-router-dom';
 import { getAllCars } from '../redux/actions/carsActions';
 import Spinner from '../components/Spinner';
 import moment from 'moment'
 import { bookCar } from '../redux/actions/bookingActions'
-const{RangePicker}= DatePicker;
+import StripeCheckout from 'react-stripe-checkout';
+const { RangePicker } = DatePicker;
 
 
 function BookingCar() {
   const user = JSON.parse(localStorage.getItem('user'));
-  const {cars} = useSelector(state=>state.carsReducer)
-  const {loading} = useSelector(state=>state.alertsReducer)
-  const[car, setcar]=useState({})
+  const { cars } = useSelector(state => state.carsReducer)
+  const { loading } = useSelector(state => state.alertsReducer)
+  const [car, setcar] = useState({})
   const dispatch = useDispatch()
-  const[from, setFrom]=useState()
-  const[to, setTo]=useState()
-  const[totalHours , setTotalHours]=useState(0)
-  const[driver, setdriver] = useState(false)
+  const [from, setFrom] = useState()
+  const [to, setTo] = useState()
+  const [totalHours, setTotalHours] = useState(0)
+  const [driver, setdriver] = useState(false)
   const [totalAmount, setTotalAmount] = useState(0);
   const [showModal, setShowModal] = useState(false);
-  
+
 
   useEffect(() => {
     if (cars.length == 0) {
@@ -39,34 +40,39 @@ function BookingCar() {
     }
   }, [driver, totalHours]);
 
-  function selectTimeSlots(values){
+  function selectTimeSlots(values) {
     setFrom(moment(values[0]).format('MM DD yyyy HH:mm'))
     setTo(moment(values[1]).format('MM DD yyyy HH:mm'))
 
     setTotalHours(values[1].diff(values[0], 'hours'))
   }
 
-  function bookNow(){
+  function bookNow() {
+
+
+  }
+
+  function onToken(token) {
     const reqObj = {
-        car: car._id,
-        user: JSON.parse(localStorage.getItem("user"))._id,
-        
-        bookedTimeSlots: {
-          from,
-          to
-        },
-        totalHours,
-        totalAmount,
-        driverRequired: driver,
-      }
-      dispatch(bookCar(reqObj))
-  
+      token,
+      car: car._id,
+      user: JSON.parse(localStorage.getItem("user"))._id,
+
+      bookedTimeSlots: {
+        from,
+        to
+      },
+      totalHours,
+      totalAmount,
+      driverRequired: driver,
+    }
+    dispatch(bookCar(reqObj))
   }
 
   const params = useParams();
-  const {carid} = params;  
+  const { carid } = params;
 
-    return (
+  return (
     <DefaultLayout>
       {loading && <Spinner />}
       <Row
@@ -75,7 +81,7 @@ function BookingCar() {
         style={{ minHeight: "90vh" }}
       >
         <Col lg={10} sm={24} xs={24} className='p-3'>
-          <img src={car.image} className="carimg2 bs1 w-100" data-aos='flip-left' data-aos-duration='1500'/>
+          <img src={car.image} className="carimg2 bs1 w-100" data-aos='flip-left' data-aos-duration='1500' />
         </Col>
 
         <Col lg={10} sm={24} xs={24} className="text-right">
@@ -98,7 +104,7 @@ function BookingCar() {
             format="MMM DD yyyy HH:mm"
             onChange={selectTimeSlots}
           />
-          
+
           <br />
           <button
             className="btn1 mt-2"
@@ -129,32 +135,39 @@ function BookingCar() {
               </Checkbox>
 
               <h3>Total Amount : {totalAmount}</h3>
+              <StripeCheckout
+                shippingAddress
+                token={onToken}
+                currency='inr'
+                amount={totalAmount * 100}
+                stripeKey="pk_test_51M2qdGSEaaXqSwAjtOOevy7h1J6HcsbSdcNica8MyjcSEP2jrlxFnUmbiUT4Poc14RlwSJXbRZEAa9ZpV2b4Ee5C00LU5RIvco"
+              >
+                <button className="btn1" >
+                  Book Now
+                </button>
+              </StripeCheckout>
 
-              
-              <button className="btn1" onClick={bookNow}>
-                Book Now
-              </button>
-              
-              
+
+
             </div>
           )}
         </Col>
 
-        
+
       </Row>
       <Modal visible={showModal} closable={false} footer={false} title='Booked time slots'>
         {car && (<div className='p-2'>
 
-            {car.bookedTimeSlots?.map(slot=>{
-              return <button className='btn1 mt-2'>{slot.from} - {slot.to}</button>
-            })}
+          {car.bookedTimeSlots?.map(slot => {
+            return <button className='btn1 mt-2'>{slot.from} - {slot.to}</button>
+          })}
 
-            <div className='text-right mt-5'>
+          <div className='text-right mt-5'>
 
-              <button className='btn1' onClick={()=>{setShowModal(false)}}>CLOSE</button>
-            </div>
+            <button className='btn1' onClick={() => { setShowModal(false) }}>CLOSE</button>
+          </div>
 
-          </div>)}
+        </div>)}
 
       </Modal>
     </DefaultLayout>
